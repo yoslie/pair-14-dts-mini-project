e643149c81abaa0553cfe3e0c500372c22ca964e
Yoslie <yoslie.tan@gmail.com>
2022-07-20 11:39:41 +0700

    feat: update navbar component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { signOutFromApps } from "../authentication/firebase";
 
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
+
+const navigation = [
+  { name: 'Home', href: '#' },
+  { name: 'Series', href: '#' },
+  { name: 'Movies', href: '#' },
+  { name: 'New and Popular', href: '#' },
+  { name: 'My List', href: '#' }
+]
+
 const NavBar = () => {
   const navigate = useNavigate();
 
@@ -10,17 +22,121 @@ const NavBar = () => {
     navigate("/login");
   };
 
-  // @todo: update styling navbar
   return (
-    <>
-      <button
-        type="button"
-        className="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        onClick={buttonLogoutOnClickHandler}
-      >
-        Logout
-      </button>
-    </>
+    <Disclosure as="nav" className="bg-semiBlack-200 fixed w-full">
+      {({ open }) => (
+        <>
+          <div className="w-full px-2 sm:px-6 lg:px-8">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <img
+                    className="h-8 w-auto"
+                    src={process.env.PUBLIC_URL+"assets/images/logo.svg"}
+                    alt="Logo Movie"
+                  />
+                </div>
+                <div className="hidden sm:block sm:ml-6">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="text-gray-300 hover:bg-gray-900 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 space-x-2 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 sm:space-x-4">
+                <img
+                  className="h-5 w-5"
+                  src={process.env.PUBLIC_URL+"assets/icons/Search.svg"}
+                  alt="Search"
+                  aria-hidden="true"
+                />
+                <span className="text-white font-medium text-sm hidden sm:block">User</span>
+                <img
+                  className="h-5 w-5"
+                  src={process.env.PUBLIC_URL+"assets/icons/NotificationBell.svg"}
+                  alt="Notification Bell"
+                  aria-hidden="true"
+                />
+                {/* Profile dropdown */}
+                <Menu as="div" className="ml-3 relative">
+                  <div>
+                    <Menu.Button className="flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                      <span className="sr-only">Open user menu</span>
+                      <img
+                        className="h-8 w-8"
+                        src={process.env.PUBLIC_URL+"assets/icons/DefaultProfileIMG.svg"}
+                        alt="Default Profile Img"
+                      />
+                      <img
+                        className="h-3 w-3 ml-2"
+                        src={process.env.PUBLIC_URL+"assets/icons/DownArrow.svg"}
+                        alt="Down Arrow"
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-black hover:bg-gray-900 ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            type="button"
+                            onClick={buttonLogoutOnClickHandler}
+                            className="block px-4 py-2 text-sm text-white"
+                          >
+                            Logout
+                          </button>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
   );
 };
 
